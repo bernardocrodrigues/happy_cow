2d969dd4c9aa50da299102bf71a2df3160e1e762
Vitor Mello <vitorml34@gmail.com>
2019-05-19 01:38:46 -0300

    So vai

--- happy_cow/blockchain.py
+++ happy_cow/blockchain.py
@@ -2,11 +2,19 @@
 import datetime
 import requests
 
+bc_address = 'http://127.0.0.1/anything'
 
-class blockchain():
+class blockchain():    
 
     @staticmethod
-    def get_cattle(producer_id):
+    def hello_json():
+
+        response = requests.post('http://127.0.0.1/anything', data={'key':'value'})
+        print (response.content)
+    
+# Uncomment
+    @staticmethod
+    def get_all_cattle(producer_id):
 
         return [
             {'raca': 'Angus',
@@ -24,17 +32,49 @@ class blockchain():
              'mae': 345,
              'peso': 300,
              'nascimento': datetime.datetime.now()},
-
         ]
 
-    @staticmethod
-    def create_cattle(producer_id, ):
-        pass
+        # response = requests.get(
+        # bc_address, 
+        # headers={'Producer_id': producer_id}
+        # )
+        # if response.status_code == 200:
+            # print('Success!')
+        # else:
+            # print('Error!')
 
     @staticmethod
-    def transfer_cattle(origin_id, destination_id):
-        pass
+    def create_cattle(producer_id, race, father, mother, weight):
+        
+        payload_dict = {'producer_id' : producer_id,
+                        'race'        : race, 
+                        'father'      : father,
+                        'mother'      : mother,
+                        'weight'      : weight}
+
+        response = requests.post(bc_address, data = payload_dict)
+
+        if response.status_code == 200:
+            print('Success!')
+        else:
+            print('Error!')
+
+    @staticmethod
+    def transfer_cattle(origin_id, cattle_id, weight, destination_id):
+        payload_dict = {'origin_id'      : origin_id,
+                        'cattle_id'      : cattle_id, 
+                        'weight'         : weight,
+                        'destination_id' : destination_id,
+                        }
+
+        response = requests.post(bc_address, data = payload_dict)
+
+        if response.status_code == 200:
+            print('Success!')
+        else:
+            print('Error!')
 
 
 if __name__ == "__main__":
-    pass
+    # blockchain.hello_json()
+    blockchain.create_cattle("jao", "nova", "joo", "jaa", "86")
